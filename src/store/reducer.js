502c865d52edd672357f94d0134c473e16f8d1f8
beadth <73101124+beadth@users.noreply.github.com>
2020-10-30 18:10:17 +0800

    init

--- src/store/reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux-immutable';
-import { reducer as sp500Reducer } from '../sp500/store';
-
-const reducer = combineReducers({
-	// vipmap: vipmapReducer,
-	sp500: sp500Reducer,
-});
-
-export default reducer;
