6985ae15e036faf28d11525b7c533ba1acb2fa49
beadth <73101124+beadth@users.noreply.github.com>
2020-11-01 20:16:54 +0800

    chore page

--- src/sp500/index.js
+++ src/sp500/index.js
@@ -120,11 +120,8 @@ const SP500UI = (props) => {
 
     <Layout>
       <Header className="header">
-        <div className="logo"/>
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
+          <Menu.Item key="1">Market Breadth</Menu.Item>
         </Menu>
       </Header>
       <Row justify="center">
