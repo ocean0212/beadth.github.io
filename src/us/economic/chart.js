1e9655bbb8b41a36216c47f7c4cb0e81c1f5102e
beadth <73101124+beadth@users.noreply.github.com>
2020-12-03 14:19:33 +0800

    close #7

--- src/us/economic/chart.js
+++ src/us/economic/chart.js
@@ -54,10 +54,31 @@ export const MarketSomaHoldChart = (props) => {
 
 
 export const WeiChart = (props) => {
+  const minVal = props.weiDatalist.min("value")
+  const maxVal = props.weiDatalist.max("value")
+  const last_two = props.weiDatalist.rows[props.weiDatalist.rows.length-2]
+  const current = props.weiDatalist.rows[props.weiDatalist.rows.length-1]
+  var flagStr = "▲"
+  var flagClo = "#0B6121"
+  if (current.value < last_two.value){
+    flagStr = "▼"
+    flagClo = "#FF0000"
+  }
+
+
+  for (var i=0; i<props.weiDatalist.rows.length;i++){
+    if (props.weiDatalist.rows[i].value === minVal){
+      var minTime = props.weiDatalist.rows[i].time
+    }
+    if (props.weiDatalist.rows[i].value === maxVal){
+      var maxTime = props.weiDatalist.rows[i].time
+    }
+  }
 
   const scale = {
     value:{
       min: -15,
+      max: 8,
       alias: '经济指数'
     },
     time:{
@@ -65,10 +86,90 @@ export const WeiChart = (props) => {
     }
   }
 
+
+
+
   return (
     <div>
-      <Chart padding={[10, 20, 70, 40]} scale={scale} autoFit height={300} data={props.weiDatalist.rows}>
+      <Chart padding={[10, 20, 70, 40]} scale={scale} autoFit height={500} data={props.weiDatalist.rows}>
         <Line shape="line" position="time*value" color="type" />
+        <Annotation.DataMarker
+          position={[current.time, current.value]}
+          text={{
+            content: flagStr + current.value,
+            style: {
+              textAlign: 'right',
+              fontSize: 13,
+              fill: flagClo,
+            },
+          }}
+          line={{
+            length: 20,
+          }}
+          direction="upward"
+        />
+        <Annotation.DataMarker
+          position={[minTime, minVal]}
+          text={{
+            content: 'Low: ' + minVal,
+            style: {
+              textAlign: 'right',
+              fontSize: 13
+            },
+          }}
+          line={{
+            length: 10,
+          }}
+          direction="downward"
+        />
+        <Annotation.DataMarker
+          position={[maxTime, maxVal]}
+          text={{
+            content: 'Hi: ' + maxVal,
+            style: {
+              textAlign: 'left',
+              fontSize: 13
+            },
+          }}
+          line={{
+            length: 20,
+          }}
+          direction="upward"
+        />
+        <Annotation.Line
+          start={['min', 2]}
+          end={['max', 2]}
+          style={{
+            lineDash: [4, 2],
+            stroke: '#FF4D4F'
+          }}
+          text={{
+            position: 'start',
+            content: '2.00 ▼',
+            style: {
+              fill: '#FF4D4F'
+            },
+            offsetX: -20,
+            offsetY: -5,
+          }}
+        />
+        <Annotation.Line
+          start={['min', -3.76]}
+          end={['max', -3.76]}
+          style={{
+            lineDash: [4, 2],
+            stroke: '#0080FF'
+          }}
+          text={{
+            position: 'start',
+            content: '-3.76 ▼',
+            style: {
+              fill: '#0080FF'
+            },
+            offsetX: -20,
+            offsetY: -5,
+          }}
+        />
         <Annotation.Text {...CHART_COPYRIGHT}/>
       </Chart>
     </div>)
